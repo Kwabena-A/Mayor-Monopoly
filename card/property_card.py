fe658862a92ed3c8283530d16d55f023ef535409
Kwabena Aboagye <Kwabena14817@gmail.com>
2026-08-22 10:27:00 +0400

    Replaced and cleaned original board

--- card/property_card.py
+++ card/property_card.py
@@ -1,10 +1,11 @@
 from .card import Card
 
 class PropertyCard(Card):
-    def __init__(self, name, price, rent):
-        super().__init__(name, "☐")
-        self.price = price
-        self.rent = rent
+    def __init__(self, **kwargs):
+        print(kwargs)
+        super().__init__(kwargs["Name"], "☐")
+        self.info = kwargs
+
 
     def land_on(self): # Dosent account for own player yet.
         if super().ownership == False:
